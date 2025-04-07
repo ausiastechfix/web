eb814565ff7056a77e5745ec97fd9d3b624dc229
ausiastechfix <techfix@ausiasmarch.net>
2025-04-07 19:57:51 +0200

    Desactivado el javascript del menú

--- js.js
+++ js.js
@@ -3,7 +3,7 @@
   var menuBox = document.getElementById('menu-box');  
 
   if(menuBox.style.display == "table-row") { // if is menuBox displayed, hide it
-    menuBox.style.display = "none";
+// DISABLED    menuBox.style.display = "none";
 
   }
   else { // if is menuBox hidden, display it
